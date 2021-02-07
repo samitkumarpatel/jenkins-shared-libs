77a2a27bd92f9a95456d85bdd50c124ea30d3818
samitkumarpatel <samitkumarpatel@gmail.com>
2021-02-07 21:02:05 +0100

    some more global library added for poc

---
+++ src/net/samittutorial/Log.groovy
@@ -0,0 +1,14 @@
+package net.samittutorial
+
+class Log {
+    def steps;
+    Log(steps) {
+        this.steps = steps
+    }
+    def info(message) {
+        steps.echo "INFO : ${message}"
+    }
+    def debug(message) {
+        steps.echo "DEBUG: ${message}"
+    }
+}
