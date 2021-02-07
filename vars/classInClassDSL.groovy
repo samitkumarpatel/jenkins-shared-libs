dc92ec79e1ba0a4095bad7754f6b9b9715b447a3
samitkumarpatel <samitkumarpatel@gmail.com>
2021-02-07 23:23:54 +0100

    import erro -fix02

--- vars/classInClassDSL.groovy
+++ vars/classInClassDSL.groovy
@@ -3,7 +3,7 @@ import net.samittutorial.ClassInClass
 def call() {
     def classInClass = new ClassInClass()
     print classInClass.show(
-                    new ClassInClass().InnerClass(id: 1, name: "FROM DSL")
+                    new ClassInClass.InnerClass(id: 1, name: "FROM DSL")
 
     )
 }
